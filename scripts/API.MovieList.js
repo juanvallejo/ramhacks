8c2cc93ab126cab0e9383b7552dcf5d71555fbab
LordFluffyPants <rixmaddi@gmail.com>
2014-11-09 05:47:44 -0500

    Updated the view API to properly display information

--- scripts/API.MovieList.js
+++ scripts/API.MovieList.js
@@ -18,20 +18,24 @@
 		
 		var movieQuery = queryName.trim();				//Movie Title & Actor Name Search String
 		movieQuery = movieQuery.replace(' ', '+');	//convert spaces to %20
-		
-		
-		httpRequest('http://navigator-fixed.rhcloud.com/apis/http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=csu7tr3mhfw52dge9tqw5cwf&q='+ queryName +'&page_limit=5', function(MovieContent) {
-		
-		MovieContent2 = JSON.parse(MovieContent);
-		console.log(MovieContent2);
-		var products = [];
-		for (var i=0; i < MovieContent2.movies.length; i++)
-		{		
-			products.push(MovieContent2.movies[i]);
-		}
-		// return products
-			callback.call(this, products);
+			
+		httpRequest('http://www.imdbapi.com/?t='+movieQuery+'+&y=&plot=short&r=json', function(MovieContent){
+			var products = [];
+			products.push(JSON.parse(MovieContent));
+			callback.call(this,products);
 		});
+		// httpRequest('http://navigator-fixed.rhcloud.com/apis/http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=csu7tr3mhfw52dge9tqw5cwf&q='+ queryName +'&page_limit=5', function(MovieContent) {
+		
+		// MovieContent2 = JSON.parse(MovieContent);
+		// console.log(MovieContent2);
+		// var products = [];
+		// for (var i=0; i < MovieContent2.movies.length; i++)
+		// {		
+		// 	products.push(MovieContent2.movies[i]);
+		// }
+		// return products
+		// 	callback.call(this, products);
+		// });
 
 	}
 	
