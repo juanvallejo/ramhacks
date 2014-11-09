84a273116458598b46b4b791b2870c342b76e9f2
juanvallejo <juan.vallejo.12@cnu.edu>
2014-11-08 21:35:50 -0500

    add externalAPIS Nodejs Server Service

---
+++ scripts/Service.externalAPIS.js
@@ -0,0 +1,19 @@
+/**
+* Provided under the MIT License (c) 2014
+* See LICENSE @file for details.
+*
+* @file Service.externalAPIS.js
+*
+* @author juanvallejo
+* @date 10/15/14
+*
+* Node.js server
+* Holds all external api library variables. These are loaded through external links
+* 
+*/
+
+// define required server vars
+
+// load amazon products api
+var aws = require('aws-lib');
+
